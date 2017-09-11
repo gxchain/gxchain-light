0eaffe3bcf9cf31bb3589710e9940704491e4a75
徐磊 <xulei@gxb.io>
2017-09-11 11:41:49 +0800

    update module: explorer-statistics - language/en

--- web/app/components/Explorer/Statistics.jsx
+++ web/app/components/Explorer/Statistics.jsx
@@ -18,8 +18,8 @@ class Statistics extends React.Component {
 
     componentDidMount() {
         let self = this;
-        document.getElementById('bodyBox').parentNode.parentNode.onscroll = function(){
-            let current_y = document.getElementById('bodyBox').parentNode.parentNode.scrollTop;
+        document.getElementById('bodyBox').parentNode.onscroll = function(){
+            let current_y = document.getElementById('bodyBox').parentNode.scrollTop;
             let pg0_h = document.getElementById("page0").offsetHeight - 62;
 
             if (current_y < pg0_h){
@@ -48,27 +48,27 @@ class Statistics extends React.Component {
     scroll_To = (tar_y) => { //tar_y 即滑动条顶端 距离页面最上面的距离
         let self = this;
         let timer = setTimeout(function(){
-            let current_y = document.getElementById('bodyBox').parentNode.parentNode.scrollTop
+            let current_y = document.getElementById('bodyBox').parentNode.scrollTop
             const step = 25 //步长系数 即剩余的距离除以40 每1ms 移动一段距离
             if (tar_y>=current_y){ //tar_y > current_y 即向下滚动
                 let next_y = current_y + step
                 if(next_y<=tar_y){  //向上滚动和向下滚动判定的区别 是这里！！
-                    document.getElementById('bodyBox').parentNode.parentNode.scrollTop = next_y;
+                    document.getElementById('bodyBox').parentNode.scrollTop = next_y;
                     self.scroll_To(tar_y)
                 }
                 else{
-                    document.getElementById('bodyBox').parentNode.parentNode.scrollTop = tar_y;
+                    document.getElementById('bodyBox').parentNode.scrollTop = tar_y;
                     clearTimeout(timer)
                 }
             }
             else{ //tar_y < current_y 即向上滚动
                 let next_y = current_y - step
                 if(next_y>=tar_y){
-                    document.getElementById('bodyBox').parentNode.parentNode.scrollTop = next_y;
+                    document.getElementById('bodyBox').parentNode.scrollTop = next_y;
                     self.scroll_To(tar_y)
                 }
                 else{
-                    document.getElementById('bodyBox').parentNode.parentNode.scrollTop = tar_y;
+                    document.getElementById('bodyBox').parentNode.scrollTop = tar_y;
                     clearInterval(timer)
                 }
             }
