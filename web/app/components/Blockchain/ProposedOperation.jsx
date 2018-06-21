8d1072bc4c8e3a6bfbef16aef3c07bab3334412a
lanhaoxiang <lanhaoxiang@qq.com>
2018-06-21 19:35:06 +0800

    support proposal operation for upgrade account

--- web/app/components/Blockchain/ProposedOperation.jsx
+++ web/app/components/Blockchain/ProposedOperation.jsx
@@ -290,22 +290,14 @@ class ProposedOperation extends React.Component {
                 break;
 
             case "account_upgrade":
-                if (op[1].upgrade_to_lifetime_member) {
-                    column = (
-                        <span>
-                       {this.linkToAccount(op[1].account_to_upgrade) } &nbsp;
-                            <Translate component="span" content="proposal.lifetime_upgrade_account"/>
-                       </span>
-                    );
-                } else {
-                    column = (
-                        <span>
-                       {this.linkToAccount(op[1].account_to_upgrade) } &nbsp;
-                            <Translate component="span" content="proposal.annual_upgrade_account"/>
-                       </span>
-                    );
-
-                }
+                return  <span>
+                            <TranslateWithLinks
+                                string={op[1].upgrade_to_lifetime_member ? "proposal.lifetime_upgrade_account" : "proposal.annual_upgrade_account"}
+                                keys={[
+                                    {type: "account", value: op[1].account_to_upgrade, arg: "account"}
+                                ]}
+                            />
+                        </span>
                 break;
 
             case "account_transfer":
