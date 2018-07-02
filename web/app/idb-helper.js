4a988662b42e363c603cf4cf62215f7aa3fd6aa9
lanhaoxiang <lanhaoxiang@qq.com>
2018-07-02 13:59:58 +0800

    ChainEvent definition

--- web/app/idb-helper.js
+++ web/app/idb-helper.js
@@ -34,31 +34,31 @@ export default idb_helper = {
     },
 
     /** Chain an add event.  Provide the @param store and @param object and
-        this method gives you convenient hooks into the database events.
+     this method gives you convenient hooks into the database events.
 
-        @param event_callback (within active transaction)
-        @return Promise (resolves or rejects outside of the transaction)
-    */
+     @param event_callback (within active transaction)
+     @return Promise (resolves or rejects outside of the transaction)
+     */
     add: (store, object, event_callback) => {
-        return function(object, event_callback) {
+        return function (object, event_callback) {
             let request = store.add(object);
             let event_promise = null;
-            if(event_callback)
+            if (event_callback)
                 request.onsuccess = new ChainEvent(
                     request.onsuccess, event => {
                         event_promise = event_callback(event);
                     }).event;
 
-            let request_promise = idb_helper.on_request_end(request).then( event => {
+            let request_promise = idb_helper.on_request_end(request).then(event => {
                 //DEBUG console.log('... object',object,'result',event.target.result,'event',event)
-                if ( event.target.result != void 0) {
+                if (event.target.result != void 0) {
                     //todo does event provide the keyPath name? (instead of id)
                     object.id = event.target.result;
                 }
-                return [ object, event ];
+                return [object, event];
             });
 
-            if(event_promise)
+            if (event_promise)
                 return Promise.all([event_promise, request_promise]);
             return request_promise;
 
@@ -67,8 +67,8 @@ export default idb_helper = {
 
     /** callback may return <b>false</b> to indicate that iteration should stop */
     cursor: (store_name, callback, transaction) => {
-        return new Promise((resolve, reject)=>{
-            if( ! transaction) {
+        return new Promise((resolve, reject) => {
+            if (!transaction) {
                 transaction = db.transaction(
                     [store_name], "readonly"
                 );
@@ -83,8 +83,8 @@ export default idb_helper = {
             request.onsuccess = e => {
                 let cursor = e.target.result;
                 let ret = callback(cursor, e);
-                if(ret === false) resolve();
-                if(!cursor) resolve(ret);
+                if (ret === false) resolve();
+                if (!cursor) resolve(ret);
             };
             request.onerror = (e) => {
                 let error = {
@@ -100,21 +100,23 @@ export default idb_helper = {
 
     autoIncrement_unique: (db, table_name, unique_index) => {
         return db.createObjectStore(
-            table_name, { keyPath: "id", autoIncrement: true }
+            table_name, {keyPath: "id", autoIncrement: true}
         ).createIndex(
-            "by_"+unique_index, unique_index, { unique: true }
+            "by_" + unique_index, unique_index, {unique: true}
         );
     }
 };
 
 class ChainEvent {
     constructor(existing_on_event, callback, request) {
-        this.event = (event)=> {
-            if(event.target.error)
+        this.callback = callback;
+        this.existing_on_event = existing_on_event;
+        this.event = (event) => {
+            if (event.target.error)
                 console.error("---- transaction error ---->", event.target.error);
             //event.request = request
-            callback(event);
-            if(existing_on_event) existing_on_event(event);
+            this.callback && this.callback(event);
+            if (this.existing_on_event) this.existing_on_event(event);
         };
     }
 }
