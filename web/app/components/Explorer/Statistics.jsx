628f9dbf2220078111611b7a3b0b1b95d0ca4b96
xLogic <xloigc@qq.com>
2018-07-13 12:25:22 +0800

    remove un useful func for data products statistics

--- web/app/components/Explorer/Statistics.jsx
+++ web/app/components/Explorer/Statistics.jsx
@@ -19,26 +19,11 @@ class Statistics extends React.Component {
                 transaction_today_count: 0,
                 transaction_pay_fees: 0,
                 merchants_total_count: 0
-            },
-            currentIndex: 0,
+            }
         };
     }
     componentDidMount() {
         let self = this;
-        if (document.getElementById("bodyBox")) {
-            document.getElementById("bodyBox").parentNode.onscroll = function () {
-                let current_y = document.getElementById("bodyBox").parentNode.scrollTop;
-                let pg0_h = document.getElementById("page0").offsetHeight;
-
-                if (current_y < pg0_h) {
-                    self.setState({currentIndex: 0});
-                }
-                if (current_y >= pg0_h) {
-                    self.setState({currentIndex: 1});
-                }
-            };
-        }
-
         const xhr = new XMLHttpRequest();
         xhr.open("GET", SettingsStore.getSetting("statistics_address"), true);
         // xhr.withCredentials = true
@@ -75,58 +60,10 @@ class Statistics extends React.Component {
             }
         };
     }
-    handleClick = (e) => {
-        let currentIndex = e.target.getAttribute("data-index");
-        let pg0_h = document.getElementById("page0").offsetHeight;
-        this.setState({currentIndex: currentIndex});
-        switch (currentIndex) {
-            case "0":
-                this.scroll_To(0);
-                break;
-            case "1":
-                this.scroll_To(pg0_h);
-                break;
-        }
-    }
-    scroll_To = (tar_y) => { //tar_y 即滑动条顶端 距离页面最上面的距离
-        let self = this;
-        let timer = setTimeout(function () {
-            let current_y = document.getElementById("bodyBox").parentNode.scrollTop;
-            const step = 25; //步长系数 即剩余的距离除以40 每1ms 移动一段距离
-            if (tar_y >= current_y) { //tar_y > current_y 即向下滚动
-                let next_y = current_y + step;
-                if (next_y <= tar_y) {  //向上滚动和向下滚动判定的区别 是这里！！
-                    document.getElementById("bodyBox").parentNode.scrollTop = next_y;
-                    self.scroll_To(tar_y);
-                }
-                else {
-                    document.getElementById("bodyBox").parentNode.scrollTop = tar_y;
-                    clearTimeout(timer);
-                }
-            }
-            else { //tar_y < current_y 即向上滚动
-                let next_y = current_y - step;
-                if (next_y >= tar_y) {
-                    document.getElementById("bodyBox").parentNode.scrollTop = next_y;
-                    self.scroll_To(tar_y);
-                }
-                else {
-                    document.getElementById("bodyBox").parentNode.scrollTop = tar_y;
-                    clearInterval(timer);
-                }
-            }
-        }, 1);
-    }
     render() {
         require("assets/stylesheets/components/_statistics.scss");
-        require("assets/iconfont.less"); // iconfont本地化
-        let index = this.state.currentIndex;
         return (
             <div className="home-wrapper" id="bodyBox">
-                <div className="nav-wrapper">
-                    <div className={index == 0 ? "active" : ""} data-index="0" onClick={this.handleClick}></div>
-                    <div className={index == 1 ? "active" : ""} data-index="1" onClick={this.handleClick}></div>
-                </div>
                 <div className="banner-wrapper" id="page0">
                     <div className="banner">
                         <LogoCard/>
@@ -135,11 +72,6 @@ class Statistics extends React.Component {
                             <p><Translate component="span"
                                           content="explorer.statistics.transaction_basic_subtitle"/></p>
                             <DataTransactionCard {...this.state}/>
-                        </div>
-                        <div className="banner-down-wrapper">
-                            <div className="banner-mouse">
-                                <div className="mouse-bar"></div>
-                            </div>
                         </div>
                     </div>
                 </div>
