5422ca747c045be02f9236fba4e6cfd767575197
yrm <yrm@outlook.com>
2020-02-13 20:33:05 +0800

    feat: set commission rate and trustnode unbanned

--- app/components/Account/GXBAccountMembership.jsx
+++ app/components/Account/GXBAccountMembership.jsx
@@ -5,6 +5,7 @@ import AccountActions from "actions/AccountActions";
 import Translate from "react-translate-component";
 import {ChainStore} from "gxbjs/es";
 import utils from "common/utils";
+import cname from "classnames";
 
 class GXBAccountMembership extends React.Component {
 
@@ -24,21 +25,33 @@ class GXBAccountMembership extends React.Component {
         this.state = {
             isCommittee: -1,
             isWitness: -1,
-            witness_id: '',
-            url: '',
-            signing_key: '',
+            witness_id: "",
+            url: "",
+            signing_key: "",
+            commission_rate: 0,
+            commission_update_time: "",
+            is_banned: false
         };
     }
 
     handelChange(e){
         this.setState({
             url: e.target.value
-        })
+        });
     }
     handelChangeSigningkey(e){
         this.setState({
             signing_key: e.target.value
-        })
+        });
+    }
+
+    handelChangeCommissionRate(e){
+        let value = e.target.value;
+        value = value.match(/^\d*(\.?\d{0,1})/g)[0] || "";
+        e.target.value = value;
+        this.setState({
+            commission_rate: value
+        });
     }
 
     shouldComponentUpdate(nextProps, nextState) {
@@ -53,7 +66,7 @@ class GXBAccountMembership extends React.Component {
     }
 
     loadTrustNodeStatus(props) {
-        console.log('load status');
+        console.log("load status");
         this.setState({
             isWitness: -1,
             isCommittee: -1
@@ -62,12 +75,16 @@ class GXBAccountMembership extends React.Component {
         if (props.isMyAccount) {
             let account = props.account.toJS();
             Promise.all([ChainStore.fetchWitnessByAccount(account.id), ChainStore.fetchCommitteeMemberByAccount(account.id)]).then(results => {
+                console.log(results[0]);
                 this.setState({
-                    witness_id: results[0] && results[0].get('id'),
+                    witness_id: results[0] && results[0].get("id"),
                     isWitness: !!results[0],
                     isCommittee: !!results[1],
-                    url: results[0] && results[0].get('url'),
-                    signing_key: results[0] && results[0].get('signing_key'),
+                    url: results[0] && results[0].get("url"),
+                    signing_key: results[0] && results[0].get("signing_key"),
+                    commission_rate: results[0] && results[0].get("commission_rate") / 10,
+                    commission_update_time: results[0] && results[0].get("commission_update_time"),
+                    is_banned: results[0] && results[0].get("is_banned"),
                 });
             });
         }
@@ -77,18 +94,27 @@ class GXBAccountMembership extends React.Component {
         if (account === undefined) return undefined;
         if (account === null) return "unknown";
 
-        if (account.get('membership_expiration_date') !== '1970-01-01T00:00:00') {
-            return 'lifetime';
+        if (account.get("membership_expiration_date") !== "1970-01-01T00:00:00") {
+            return "lifetime";
         } else {
-            return 'basic';
+            return "basic";
         }
     }
 
     render() {
-        console.log('render');
+        console.log("render");
         let {isMyAccount, gprops, core_asset} = this.props;
         let account = this.props.account.toJS();
 
+        let set_commission_interval = gprops.getIn([
+            "parameters",
+            "extensions",
+            2,
+            1,
+            "set_commission_interval"
+        ]);
+        let commission_update_time = new Date(this.state.commission_update_time).getTime();
+        let isCooldown = (new Date().getTime() - commission_update_time) < set_commission_interval * 1000;
         let account_name = account.name;
 
         let member_status = this.getAccountMemberStatus(this.props.account);
@@ -119,9 +145,17 @@ class GXBAccountMembership extends React.Component {
                     
                     {this.state.isWitness == 1 && this.state.isCommittee == 1 ?
                         <div>
-                            <Translate content="account.member.trust_node_candidate"/>
-                            &nbsp;{this.state.witness_id}
-                            
+                            <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                                <div><Translate content="account.member.trust_node_candidate"/>&nbsp;{this.state.witness_id}</div>
+                                {
+                                    this.state.is_banned ?
+                                    <div>
+                                        <button className="button" onClick={this.unbannedWitness.bind(this, this.state.witness_id, account)}>
+                                            <Translate content="wallet.unbanned" />
+                                        </button>
+                                    </div> : null
+                                }
+                            </div>
                             <hr/>
                             <div>
                                 <label>url</label> 
@@ -131,10 +165,27 @@ class GXBAccountMembership extends React.Component {
                                 <label>signing_key</label> 
 
                                 <input type="text" onChange={this.handelChangeSigningkey.bind(this)} defaultValue={this.state.signing_key}/>
+                                <div>
+                                    <button className="button" onClick={this.updateWitness.bind(this, this.state.witness_id, account, this.state.url, this.state.signing_key)}>
+                                        <Translate content="wallet.submit" />
+                                    </button>
+                                </div>
+                            </div>
 
-                                <button className="button" onClick={this.updateWitness.bind(this, this.state.witness_id, account, this.state.url, this.state.signing_key)}>
-                                    <Translate content="wallet.submit" /> 
-                                </button>
+                            <hr/>
+
+                            <div>
+                                <label>Commission Rate</label>
+                                <div style={{display: "flex",alignItems: "center"}}>
+                                    <input type="number" min="0" max="100" onChange={this.handelChangeCommissionRate.bind(this)} defaultValue={this.state.commission_rate} /><span>%</span>
+                                </div>
+                                <div>
+                                    <button className={cname("button", {
+                                        disabled: isCooldown
+                                    })} onClick={this.updateCommissionRate.bind(this, this.state.witness_id, account, this.state.commission_rate)}>
+                                        <Translate content="wallet.submit" />
+                                    </button>
+                                </div>
                             </div>
                                 
                         </div> 
@@ -168,6 +219,14 @@ class GXBAccountMembership extends React.Component {
     updateWitness(witness_id, account, url, signing_key, e) {
         e.preventDefault();
         AccountActions.updateWitness(witness_id, account.id, signing_key, url);
+    }
+    updateCommissionRate(witness_id, account, rate, e) {
+        e.preventDefault();
+        AccountActions.updateCommissionRate(witness_id, account.id, rate * 10);
+    }
+    unbannedWitness(witness_id, account, e) {
+        e.preventDefault();
+        AccountActions.unbannedWitness(witness_id, account.id);
     }
 
 
